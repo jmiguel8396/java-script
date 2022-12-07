2935c62b765b0aef1789490d527091cd577b18ed
jmiguel8396 <jmiguel_8396@hotmail.com>
2022-12-07 13:56:20 -0300

    new

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia!
+        img.src = 'imagem/fotomanha.png'
+        document.body.style.background = '#c0aa77'
+    } else if (hora >= 12 && hora < 18) {
+        //Boa tarde!
+        img.src = 'imagem/fototarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        //Boa noite!
+        img.src = 'imagem/fotonoite.png'
+        document.body.style.background = '#03263c'
+    }
+}
